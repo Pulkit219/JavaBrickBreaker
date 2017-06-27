1b18c86a4be4c5adce4fc80997be530ce8a888f6
Pulkit219 <ascension1110@gmail.com>
2017-06-27 14:03:14 -0400

    3rd comm
    designing panel

--- src/brickGamePackage/Gamepanel.java
+++ src/brickGamePackage/Gamepanel.java
@@ -1,5 +1,7 @@
 package brickGamePackage;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -7,8 +9,49 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class Gamepanel extends JPanel implements KeyListener, ActionListener{
+import com.sun.glass.ui.Timer;
 
+public class Gamepanel extends JPanel implements KeyListener, ActionListener{
+	private boolean play = false;
+	private int score =0;
+	
+	private int totalBricks =21;
+	
+	private Timer timer;
+	private int delay = 8;
+	
+	private int playerX =310;
+	
+	private int ballposX =120;
+	private int ballposY = 350;
+	private int ballXdir =-1;
+	private int ballYdir =-2;
+	
+	public Gamepanel ()
+	{
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(true);
+//		timer = new Timer(delay, this);
+//		timer.start();
+	}
+	
+public void paint(Graphics g)
+
+{
+	//background
+	g.setColor(Color.black);
+	g.fillRect(1, 1, 692, 592);
+	
+	
+	//borders
+	g.setColor(Color.YELLOW);
+	g.fillRect(0, 0, 3, 592);
+	g.fillRect(0, 0, 692, 3);
+	g.fillRect(691, 0, 3, 592);
+
+	
+}
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
