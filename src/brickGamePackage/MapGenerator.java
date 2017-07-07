fdfe26a8f45c7491ea9fd6ac2bca653a3f39622a
Pulkit219 <ascension1110@gmail.com>
2017-07-06 22:45:59 -0400

    10th comm
    working on bricks

--- src/brickGamePackage/MapGenerator.java
+++ src/brickGamePackage/MapGenerator.java
@@ -1,5 +1,13 @@
 package brickGamePackage;
 
 public class MapGenerator {
-
+	public int map[][];
+	public int brickWidth;
+	public int brickHeight;
+	
+	
+	public MapGenerator(int row, int column)
+	{
+		map = new int[row][column];
+	}
 }
