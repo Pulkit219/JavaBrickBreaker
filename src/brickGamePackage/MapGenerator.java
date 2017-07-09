74d95f94423ca104f8bfb0a7e9449a070fad0601
Pulkit219 <ascension1110@gmail.com>
2017-07-08 20:24:25 -0400

    11th comm
    looping on bricks

--- src/brickGamePackage/MapGenerator.java
+++ src/brickGamePackage/MapGenerator.java
@@ -1,13 +1,39 @@
 package brickGamePackage;
 
+import java.awt.Graphics2D;
+
 public class MapGenerator {
 	public int map[][];
 	public int brickWidth;
 	public int brickHeight;
 	
 	
-	public MapGenerator(int row, int column)
+	public MapGenerator(int row, int col)
 	{
-		map = new int[row][column];
+		map = new int[row][col];
+		for(int i =0; i<map.length;i++)
+		{
+			for(int j=0;j<map[0].length;j++)
+			{
+				map[i][j]=1;
+			}
+		}
+		brickHeight = 150/row;
+		brickWidth = 540/col;
+	}
+	
+	public void draw(Graphics2D g )
+	{
+		for(int i =0; i<map.length;i++)
+		{
+			for(int j=0;j<map[0].length;j++)
+			{
+				if(map[i][j]>0)
+				{
+					
+				}
+			}
+		}
+		
 	}
 }
